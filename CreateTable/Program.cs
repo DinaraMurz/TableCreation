ad714beed19106ced0e7975cd369042d99e18c46
Динара Еркинкызы Мырзабек <@corp.itstep>
2019-04-25 20:35:13 +0600

    first commit

---
+++ CreateTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\МырзабекДи\source\repos\CreateTable\CreateTable\Database.mdf;Integrated Security=True");
+            string query =
+            @"CREATE TABLE [dbo].[Gruppa]
+                (
+	                Id int IDENTITY(1,1) PRIMARY KEY,
+	                Name nvarchar(MAX) NOT NULL
+                );";
+            SqlCommand cmd = new SqlCommand(query, conection);
+            try
+            {
+                conection.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Таблица создана");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Ошибка: " + exception.ToString());
+            }
+
+             conection.Close();
+             Console.ReadLine();
+        }
+    }
+}
